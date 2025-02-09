0466071b2fa4039adbbef73a6723d62e64891637
koei-kaji <koei.kaji@gmail.com>
2025-02-09 18:28:01 +0900

    feat: gitlinker を追加

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -8,4 +8,13 @@ return {
 			require("diffview").setup()
 		end,
 	},
+	{
+		"linrongbin16/gitlinker.nvim",
+		cmd = "GitLink",
+		opts = {},
+		keys = {
+			{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+			{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+		},
+	},
 }
