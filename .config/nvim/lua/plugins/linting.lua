a11bdd9295b58bea1ba2d673a461a43dbc1a1a5e
koei-kaji <koei.kaji@gmail.com>
2025-02-12 09:19:59 +0900

    feat: add nvim-lint

---
+++ .config/nvim/lua/plugins/linting.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			local lint = require("lint")
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+
+			-- https://github.com/mfussenegger/nvim-lint/issues/559#issuecomment-2263995711
+			-- Show linters for the current buffer's file type
+			vim.api.nvim_create_user_command("LintInfo", function()
+				local filetype = vim.bo.filetype
+				local linters = require("lint").linters_by_ft[filetype]
+
+				if linters then
+					print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+				else
+					print("No linters configured for filetype: " .. filetype)
+				end
+			end, {})
+		end,
+	},
+}
