89caf43ef5f3a0fa347322df77fa0a80f1a841d4
koei-kaji <koei.kaji@gmail.com>
2025-03-11 09:17:32 +0900

    chore: small update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,25 +12,26 @@ config.default_prog = {
 	"-l",
 	"/opt/homebrew/bin/tmux",
 }
+
 config.initial_cols = 150
 config.initial_rows = 50
-
+config.window_decorations = "RESIZE"
 config.color_scheme = "Tokyo Night"
-config.window_background_opacity = 0.5
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
-config.macos_window_background_blur = 30
+
 config.window_frame = {
 	font = wezterm.font("Moralerspace Argon NF"),
 	font_size = 15.0,
 }
-config.window_decorations = "RESIZE"
-
 config.font = wezterm.font("Moralerspace Argon NF")
 config.font_size = 18.0
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
 
 return config
