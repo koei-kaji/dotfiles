9fef9f63b942d775b844c9f6f5421326a3a59ad9
koei-kaji <koei.kaji@gmail.com>
2026-05-07 08:55:39 +0900

    chore: update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -55,6 +55,34 @@ return {
           end
         end)
       end
+
+      -- Workaround: definitionProvider がない buffer で peek_definition を呼ぶと
+      -- lspsaga 側の pending_request が戻らず、以後ずっと "already been sent"
+      -- になるため、未対応 buffer ではリクエスト自体を送らない。
+      local definition = require("lspsaga.definition")
+      local raw_definition_request = definition.definition_request
+      function definition:definition_request(method, handler_T, args)
+        local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf(), method = method })
+        if vim.tbl_isempty(clients) then
+          self.pending_request = false
+          vim.notify("[lspsaga] no LSP client supports " .. method, vim.log.levels.WARN)
+          return
+        end
+
+        local request_token = {}
+        self._dotfiles_request_token = request_token
+        local ok, err = pcall(raw_definition_request, self, method, handler_T, args)
+        if not ok then
+          self.pending_request = false
+          error(err)
+        end
+
+        vim.defer_fn(function()
+          if self._dotfiles_request_token == request_token and self.pending_request then
+            self.pending_request = false
+          end
+        end, 10000)
+      end
     end,
   },
   {
